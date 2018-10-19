0b29a4c4f270351451b3402a1cd4af3ed971fa7f
Manuel Torrez <manuel.torrez.vado@protonmail.com>
2018-10-19 08:20:15 -0600

    Getting input values

--- app.js
+++ app.js
@@ -6,17 +6,43 @@ let budgetController = (function() {
 
 //UI Controller 
 let UIController = (function() {
-    //TODO
+
+    let DOMstrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn'
+    }
+    
+    return {
+        getInput() {
+            return {
+                type: document.querySelector(DOMstrings.inputType).value, //Either income or expense
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value,
+            }
+        },
+
+        getDOMstrings() {
+            return DOMstrings;
+        }
+    }
+
 }());
 
 
 //Global controller
 let controller = (function(budgetCtrl, UICtrl) {
 
+    let DOM = UICtrl.getDOMstrings();
+
     //Function to follow DRY principle
     let ctrlAddItem = function() {
         //1. Get the field input data
-    
+        let input = UIController.getInput();
+        console.log(input);
+
+
         //2. Add item to the budget controller
     
         //3. Add item to the UI
@@ -24,11 +50,9 @@ let controller = (function(budgetCtrl, UICtrl) {
         //4. Calculate the budget
     
         //5. Display the budget on the UI
-
-        console.log('IT FUCKING WORKS');
     }
 
-    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
 
     document.addEventListener('keypress', function(event) {
         //keyCode 13 is the code for "Enter" key
