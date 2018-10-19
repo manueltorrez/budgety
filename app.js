578285a462435fdde8304522dd570a56c673f600
Manuel Torrez <manuel.torrez.vado@protonmail.com>
2018-10-19 07:43:45 -0600

    Add keypress listener and reusable ufnction

--- app.js
+++ app.js
@@ -1,32 +1,40 @@
+//Budget controller 
 let budgetController = (function() {
-
-    let x = 23;
-
-    let add = function(a) {
-        return x + a;
-    }
-
-    return {
-        publicTest(b) {
-            return add(b);
-        }
-    }
+    //TODO
 }());
 
 
+//UI Controller 
 let UIController = (function() {
     //TODO
 }());
 
 
+//Global controller
 let controller = (function(budgetCtrl, UICtrl) {
 
-    let z = budgetCtrl.publicTest(3);
+    //Function to follow DRY principle
+    let ctrlAddItem = function() {
+        //1. Get the field input data
+    
+        //2. Add item to the budget controller
+    
+        //3. Add item to the UI
+    
+        //4. Calculate the budget
+    
+        //5. Display the budget on the UI
 
-    return {
-        anotherPublic() {
-            console.log(z);
-        }
+        console.log('IT FUCKING WORKS');
     }
+
+    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+
+    document.addEventListener('keypress', function(event) {
+        //keyCode 13 is the code for "Enter" key
+        if(event.keyCode === 13 || event.which === 13) {
+            ctrlAddItem();
+        }
+    });
 
 }(budgetController, UIController));
