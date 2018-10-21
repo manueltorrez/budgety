d407592484eafb5ce1ddba42e7d41d6dcdb63a38
Manuel Torrez <manuel.torrez.vado@protonmail.com>
2018-10-21 07:16:49 -0600

    Creating an initialization function

--- app.js
+++ app.js
@@ -34,7 +34,22 @@ let UIController = (function() {
 //Global controller
 let controller = (function(budgetCtrl, UICtrl) {
 
-    let DOM = UICtrl.getDOMstrings();
+    //Wraping all eventListeners in a function
+    let setupEventListeners = function() {
+        
+        let DOM = UICtrl.getDOMstrings();
+        
+        document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+    
+        document.addEventListener('keypress', function(event) {
+            //keyCode 13 is the code for "Enter" key
+            if(event.keyCode === 13 || event.which === 13) {
+                ctrlAddItem();
+            }
+        });
+
+    };
+
 
     //Function to follow DRY principle
     let ctrlAddItem = function() {
@@ -50,15 +65,17 @@ let controller = (function(budgetCtrl, UICtrl) {
         //4. Calculate the budget
     
         //5. Display the budget on the UI
+    };
+
+    return {
+        init() {
+            console.log('Application started');
+            setupEventListeners();
+        }
     }
 
-    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
-
-    document.addEventListener('keypress', function(event) {
-        //keyCode 13 is the code for "Enter" key
-        if(event.keyCode === 13 || event.which === 13) {
-            ctrlAddItem();
-        }
-    });
 
 }(budgetController, UIController));
+
+
+controller.init();
