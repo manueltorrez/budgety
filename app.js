54c3d9ee529e65676f8aa6449d86297a00d9890f
Manuel Torrez <manuel.torrez.vado@protonmail.com>
2018-10-23 07:20:10 -0600

    Fix showing percentage in UI

--- app.js
+++ app.js
@@ -156,7 +156,12 @@ let UIController = (function() {
             document.querySelector(DOMstrings.budgetLabel).textContent = obj.budget;
             document.querySelector(DOMstrings.incomeLabel).textContent = obj.totalInc;
             document.querySelector(DOMstrings.expensesLabel).textContent = obj.totalExp;
-            document.querySelector(DOMstrings.percentageLabel).textContent = obj.percentage;
+            
+            if(obj.percentage > 0) {
+                document.querySelector(DOMstrings.percentageLabel).textContent = obj.percentage + '%';
+            } else {
+                document.querySelector(DOMstrings.percentageLabel).textContent = '---';
+            }
         },
 
         clearFields() {
